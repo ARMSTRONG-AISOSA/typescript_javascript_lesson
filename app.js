569fad455ead85fcd0f7bc9916c03574392a334c
Armstrong Omoregie <ammycan6@gmail.com>
2025-03-24 11:25:11 +0100

    Third Commit: Typescript compilation.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+// Typescript to js
+// node: tsc app.ts
+// run js file in terminal: node.js
+// wacth typescript file: tsc --watch app.ts
+// Or use complile hero extension for converting.
 var message = "Hello World!";
 console.log(message);
-var messages = "Hello World!";
-console.log(messages);
+var id = 3;
+console.log(id);
+var textData = "Good";
+console.log(textData);
--- app.ts
+++ app.ts
@@ -1,5 +1,14 @@
+// Typescript to js
+// node: tsc app.ts
+// run js file in terminal: node.js
+// wacth typescript file: tsc --watch app.ts
+// Or use complile hero extension for converting.
+
 let message: string = "Hello World!";
 console.log(message);
 
-const messages: string = "Hello World!";
-console.log(messages);
+const id: number = 3;
+console.log(id);
+
+const textData: string = "Good";
+console.log(textData);
